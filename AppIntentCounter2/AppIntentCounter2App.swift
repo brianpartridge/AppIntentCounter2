13da682518ee4dcfa78a9e1e6f9fc6b5b37f9dc0
Brian Partridge <brianpartridge@squareup.com>
2024-06-24 13:12:21 -0400

    Initial Commit

---
+++ AppIntentCounter2/AppIntentCounter2App.swift
@@ -0,0 +1,17 @@
+//
+//  AppIntentCounter2App.swift
+//  AppIntentCounter2
+//
+//  Created by Brian Partridge on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct AppIntentCounter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppIntentCounter2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppIntentCounter2
+//
+//  Created by Brian Partridge on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
